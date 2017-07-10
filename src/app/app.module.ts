023e02acfc85c1a128ea2a9ffc27752aebd76761
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-07-10 14:43:36 -0400

    MaterialCss added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import 'rxjs/Rx';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
