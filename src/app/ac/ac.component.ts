bf051bd5e4b195f4bcd6f1aef607c8871e35f0dc
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-07-10 15:56:02 -0400

    Components added

---
+++ src/app/ac/ac.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ac',
+  templateUrl: './ac.component.html',
+  styleUrls: ['./ac.component.css']
+})
+export class ACComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,18 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule } from '@angular/material';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { CameraComponent } from './camera/camera.component';
+import { ACComponent } from './ac/ac.component';
+import { LightsComponent } from './lights/lights.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideMenuComponent,
+    CameraComponent,
+    ACComponent,
+    LightsComponent
   ],
   imports: [
     BrowserModule,
