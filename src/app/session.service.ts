918723b3cd88fa0c94cae1fd9f4b471f76acf289
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-07-11 05:30:55 -0400

    Session service added

--- src/app/session.service.ts
+++ src/app/session.service.ts
@@ -1,8 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
 
 @Injectable()
 export class SessionService {
 
-  constructor() { }
+  BASE_URL: string = 'http://localhost:3000';
 
+  constructor(private http: Http) { }
+
+  handleError(e) {
+    return Observable.throw(e.json().message);
+  }
+
+  signup(user) {
+    return this.http.post(`${this.BASE_URL}/signup`, user)
+      .map(res => res.json())
+      .catch(this.handleError);
+  }
+
+  login(user) {
+    return this.http.post(`${this.BASE_URL}/login`, user)
+      .map(res => res.json())
+      .catch(this.handleError);
+  }
+
+  logout() {
+    return this.http.post(`${this.BASE_URL}/logout`, {})
+      .map(res => res.json())
+      .catch(this.handleError);
+  }
+
+  isLoggedIn() {
+    return this.http.get(`${this.BASE_URL}/loggedin`)
+      .map(res => res.json())
+      .catch(this.handleError);
+  }
 }
