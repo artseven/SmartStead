f18c96cd9e476ce11a2dc9db0502e65b5c343c75
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-07-24 18:09:24 -0400

    layout

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit, Input, EventEmitter } from '@angular/core';
 
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  
+  showDialog = false;
+  editingTodo: any = null;
+  fieldValue = '';
+  todoList: any = [];
+  okButtonText = 'Create task';
 
-  constructor() { }
-
-  ngOnInit() {
+  todoDialog(todo = null) {
+    this.okButtonText = 'Create task';
+    this.fieldValue = '';
+    this.editingTodo = todo;
+    if (todo) {
+      this.fieldValue = todo.title;
+      this.okButtonText = 'Edit task';
+    }
+    this.showDialog = true;
   }
 
+  remove(index: number) {
+    this.todoList.splice(index, 1);
+  }
+
+  editTodo(title) {
+    this.editingTodo.title = title;
+  }
+
+  updateTodo(title) {
+    if (title) {
+      title = title.trim();
+      if (this.editingTodo) {
+        this.editTodo(title);
+      } else {
+        this.addTodo(title);
+      }
+    }
+    this.hideDialog();
+  }
+
+  addTodo(title) {
+    const todo = {title: title, completed: false};
+    this.todoList.push(todo);
+  }
+
+  hideDialog() {
+    this.showDialog = false;
+    this.editingTodo = null;
+    this.fieldValue = null; // make sure Input is new
+  }
+  ngOnInit(){
+
+  }
 }
