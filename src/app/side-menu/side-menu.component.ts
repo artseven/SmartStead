dc0ed04afd723b5461ba341a0834a7ea06923704
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-07-27 22:24:28 -0400

    fixing layout

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -51,7 +51,7 @@ export class SideMenuComponent implements OnInit {
   ngOnInit() {
      setInterval(() => {
           this.dynamicUrl();
-        }, 200);
+        }, 500);
   }
 
   onShoppingClicked() {
