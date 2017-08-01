679ee752b34d42e0dcda520fef21a1f771c043da
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-08-01 11:48:58 -0400

    transferring jquery into angular code

--- src/app/ac/ac.component.ts
+++ src/app/ac/ac.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 
+
+declare var jQuery: any;
+declare var $: any;
+
 @Component({
   selector: 'app-ac',
   templateUrl: './ac.component.html',
   styleUrls: ['./ac.component.css']
 })
+
+
 export class ACComponent implements OnInit {
-  
+  val: number = 10 + Math.random() * 20 ;
+  radix: number = $('.rs-tooltip-text').html();
+  string;
   constructor() { }
 
   ngOnInit() {
   }
+
+ createStaff() {
+  $('<div class="rs-gradient" />').insertBefore($('.rs-tooltip'));
+}
+
+
+ update() {
+  this.val = -230 + parseInt(this.string, this.radix * 7.5);
+  $('.rs-gradient').css({
+    background: 'hsl(' + this.val + ', 100%, 57%)'
+  });
+}
+
+
+
+$('#slider').roundSlider({
+  radius: 80,
+  width: 15,
+  min: 68,
+  max: 80,
+  circleShape: "pie",
+  handleSize: "+16",
+  handleShape: "dot",
+  sliderType: "min-range",
+  startAngle: 315,
+  value: val,
+  editableTooltip: false,
+  mouseScrollAction: true,
+  change: update,
+  drag: update,
+  create: function () {
+    createStaff();
+    update();
+  }
+
+})
+
 
 }
