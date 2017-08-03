0ca60f3c1eb34b6e3427159d993c7479e23fdbbb
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-08-03 10:33:39 -0400

    adding thermostat service

--- src/app/ac/ac.component.ts
+++ src/app/ac/ac.component.ts
@@ -9,12 +9,20 @@ import { Input, Component, OnInit } from '@angular/core';
 
 
 export class ACComponent implements OnInit {
-
+  errorMessage: string;
+  toast;
   constructor() { }
 
   ngOnInit() {
   }
 
-
+  onThermostatSubmit() {
+    this.thermostatThing.submitOff()
+      .then(() => { })
+      .catch((err) => {
+        this.errorMessage = 'Could not retrieve item details. Try again later.';
+      });
+    this.toast('Turning living-room lights OFF');
+  }
 
 }
